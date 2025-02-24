5b43cfdd5d71e39c0b763c0a8389732d44ad9397
Sai Darshan <saidar9632@gmail.com>
2025-02-24 23:21:02 +0530

    upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import threading
+import datetime
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ sensor_data = {
     "rain_detected": None,
     "soil_moisture": None,
     "water_layer": None,
+    "last_update": None
 }
 
 data_lock = threading.Lock()
@@ -20,8 +22,11 @@ def update_sensor_data():
     if request.method == 'POST':
         if request.is_json:
             with data_lock:
-                sensor_data.update(request.get_json())
-            return jsonify({"message": "Data updated"}), 200
+                data = request.get_json()
+                sensor_data.update(data)
+                # Add timestamp for when data was updated
+                sensor_data["last_update"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            return jsonify({"message": "Data updated", "timestamp": sensor_data["last_update"]}), 200
         return jsonify({"error": "Invalid JSON"}), 400
     elif request.method == 'GET':
         with data_lock:
